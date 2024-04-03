a8cc042fa4e09705ce8a8d75641d73b75eddd9bf
Andrey Yarmolenko <safeflat@gmail.com>
2024-04-03 19:23:17 +0300

    add call epk_api

---
+++ movies/scheduler/config/components/apk_service.py
@@ -0,0 +1,3 @@
+from config.components.base import env
+
+EPK_API_URL = env("EPK_API_URL", default="http://epk_api:8000")
--- movies/scheduler/mailings/tasks.py
+++ movies/scheduler/mailings/tasks.py
@@ -1,10 +1,25 @@
 import logging
+import uuid
+from urllib.parse import urljoin
 
+from django.conf import settings
+
+import requests
 from config.celery import app
 
 logger = logging.getLogger(__name__)
 
 
 @app.task(name="task_send_mailings")
-def task_send_mailings(*args, **kwargs) -> None:
-    logger.warning(",".join(str(arg) for arg in args) + "; " + ",".join(f"{k}={v}" for k, v in kwargs.items()))
+def task_send_mailings(template_id: uuid.UUID, **kwargs) -> None:
+    path: str = f'/api/v1/{kwargs.get("users_selector", {}).get("url_method")}'
+
+    requests.post(
+        urljoin(settings.EPK_API_URL, path),
+        json={
+            "template_id": template_id,
+            "subject": kwargs.get("name"),
+            "type": "email",
+            "user_id": kwargs.get("users_selector", {}).get("user_id"),
+        },
+    )
